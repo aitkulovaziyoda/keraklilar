2703ced6d1f470be3502e5ebb8cfb94b530ee8af
aitkulovaziyoda <166129091+aitkulovaziyoda@users.noreply.github.com>
2024-06-17 10:41:17 +0500

    Add files via upload

---
+++ kerakli/index.js
@@ -0,0 +1,13 @@
+var login = "Aitkulova Ziyoda";
+var passvord = "5555"
+var a = +prompt("Loginni to'g'ri kiriting;")
+var w = +prompt("Parolni tezda kiriting;")
+if (login == a) {
+    console.log("Login hozir to'g'ri");
+}
+else if (passvord == w) {
+    console.log("Xush kelibsiz");
+}  
+else {
+    console.log("Bunday emas Xatolik yuz berdi");
+}
